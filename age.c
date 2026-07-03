0629a88814730d911daa23a745e9551f5787f7f2
yahya486 <neuralwave4@gmail.com>
2026-07-03 11:39:47 +0100

    Day 1 - hello world, variables, printf

---
+++ age.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ()
+{
+int age = 25;
+printf("I'm %d years old\n", age);
+return 0;
+}
---
+++ grade.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+ char grade = 'A';
+float score = 92.5;
+printf("grade: %c, score: %f\n", grade, score);
+return 0;
+}
